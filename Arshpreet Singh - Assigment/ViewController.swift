5c4b253b3706eb6e9c808aa179071643b74fb7a9
arshabhullar <arshabhullar@gmail.com>
2019-08-09 11:38:23 +0530

    Initial Commit

---
+++ Arshpreet Singh - Assigment/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Arshpreet Singh - Assigment
+//
+//  Created by Arsh Bhullar on 09/08/19.
+//  Copyright © 2019 Arsh Bhullar. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
